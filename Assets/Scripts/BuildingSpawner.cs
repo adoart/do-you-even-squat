676acb82a58fe93f8e60cf58e253f8b278606d4a
adoart <andre.pereira.duarte@gmail.com>
2020-11-30 23:54:15 +0100

    Initial commit
    Core locomotion mechanic(jump, turn and move) implemented (a bit buggy)...
    Buldings procedural generated (infinite runner).

---
+++ Assets/Scripts/BuildingSpawner.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSpawner : MonoBehaviour
+{
+    public List<GameObject> buildings;
+    public float radius;
+    public float checkRadius = 0.0f;
+    private int layerMask;
+    private readonly List<GameObject> buildingsPoll = new List<GameObject>();
+    public int pollSize;
+
+    // Start is called before the first frame update
+    void Start() {
+        // Bit shift the index of the layer (8) to get a bit mask
+        layerMask = 1 << 8;
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        layerMask = ~layerMask;
+
+        SpawnBuildings();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        SpawnBuildings();
+        CleanUpBuildings();
+        pollSize = buildingsPoll.Count;
+    }
+
+    public void DestroyBuilding(GameObject building) {
+        building.SetActive(false);
+    }
+
+    private void SpawnBuildings() {
+        foreach (GameObject building in buildings) {
+            bool spawned = false;
+            int tries = 0;
+            while (!spawned && tries <= 3) {
+                Vector2 position2D = Random.insideUnitCircle * radius;
+                Vector3 position = new Vector3(transform.position.x + position2D.x, 15.0f, transform.position.z + position2D.y);
+                float checkSphereRadius = checkRadius > 0.0f ? checkRadius : new Vector2(building.transform.localScale.x, building.transform.localScale.z).magnitude;
+                if (!Physics.CheckSphere(position, checkSphereRadius, layerMask)) {
+                    GameObject polledBuilding = buildingsPoll.Find(i => !i.activeSelf);
+                    if (polledBuilding != null) {
+                        polledBuilding.SetActive(true);
+                        polledBuilding.transform.position = position;
+                    } else {
+                        buildingsPoll.Add(Instantiate(building, position, transform.localRotation));
+                    }
+                    spawned = true;
+                } else {
+                    tries++;
+                }
+            }
+            
+        }
+    }
+
+    private void CleanUpBuildings() {
+        foreach (GameObject building in buildingsPoll) {
+            float distance = (building.transform.position - transform.position).magnitude;
+            if (distance > radius) {
+                DestroyBuilding(building);
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/JumpProvider.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class JumpProvider : MonoBehaviour
+{
+    // public variables
+    public float jumpForce = 30.0f;
+    public float forwardForce = 3.0f;
+    public float gravity = 9.81f;
+    public Transform forwardSource;
+
+    public bool grounded;
+    private Rigidbody myBody;
+    private Vector3 originalPosition;
+    public List<LocomotionProvider> locomotionProviders;
+
+    private List<InputDevice> inputDevices = new List<InputDevice>();
+    public bool isPressed = false;
+
+    // Start is called before the first frame update
+    void Start() {
+        InputDevices.GetDevices(inputDevices);
+
+        myBody = gameObject.GetComponent<Rigidbody>();
+        originalPosition = new Vector3(0.0f, 3.0f, 0.0f);
+        grounded = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Determine how much should move in the z-direction
+        Vector3 movementZ = transform.forward * forwardForce;
+        Vector3 movementY = transform.up * jumpForce;
+        Vector3 forceVector = movementY + movementZ;
+
+        Jump(forceVector);
+
+        //Debug.DrawLine(forwardSource.localPosition, Vector3.up, Color.yellow);
+        Debug.DrawLine(transform.position, forceVector, Color.red);
+        //Debug.DrawLine(forwardSource.localPosition, movementZ, Color.blue);
+
+    }
+
+    private void Jump(Vector3 forceVector) {
+
+        foreach (InputDevice inputDevice in inputDevices) {
+            if (inputDevice.isValid) {
+
+                InputHelpers.IsPressed(inputDevice, InputHelpers.Button.PrimaryButton, out isPressed);
+                if (grounded && isPressed) {
+                    Debug.Log("JUMP");
+                    myBody.isKinematic = false;
+                    foreach (LocomotionProvider locomotionProvider in locomotionProviders) {
+                        locomotionProvider.enabled = false;
+                    }
+
+                    myBody.AddForce(forceVector, ForceMode.Impulse);
+                    grounded = false;
+                }
+
+                //if vertical velocity is 0 we're grounded and we can jump again
+                if (myBody.IsSleeping()) {
+                    grounded = true;
+                    myBody.isKinematic = true;
+                    foreach (LocomotionProvider locomotionProvider in locomotionProviders) {
+                        locomotionProvider.enabled = true;
+                    }
+                }
+
+                //check if below the map, reset position
+                if (transform.position.y < -5.0f) {
+                    transform.position = originalPosition;
+                    grounded = true;
+                }
+            }
+        }
+    }
+}
